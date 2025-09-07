e2c475767c20559e632cd242dad3179de228b6cb
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-07 14:42:59 -0400

    refactor: Refactoring production code
    Generate Java code inside "SecurityConfig.java" java file to add starter code that creates security configuration class that extends "WebSecurityConfigurerAdapter" class from Spring

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/SecurityConfig.java
@@ -0,0 +1,31 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+package com.udacity.jwdnd.course1.cloudstorage;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Configure authentication provider here (e.g., in-memory, JDBC, custom user service)
+    }
+}
