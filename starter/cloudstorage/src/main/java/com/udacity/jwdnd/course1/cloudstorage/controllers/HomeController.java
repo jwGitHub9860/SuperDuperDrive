33b3aa278b23ff5ceb33403539de8035d77e0d06
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-09 21:05:29 -0400

    style: Formatting, missing semi colons, etc; no code change
    Remove one whitespace between "package" definition and "@Controller" inside "HomeController.java" file to improve code readability

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -3,7 +3,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
-
 @Controller
 @RequestMapping("/home")
 public class HomeController {
