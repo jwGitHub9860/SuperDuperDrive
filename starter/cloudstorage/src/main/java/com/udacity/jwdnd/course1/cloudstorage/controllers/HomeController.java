a0fd9f0854c293f760f0b29452987ab892a111f5
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-09 22:58:13 -0400

    refactor: Refactoring production code
    Add code that adds "credentials" attribute to home page in "getHomePage()" method inside "HomeController.java" file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -11,6 +11,7 @@ public class HomeController {
     public String getHomePage(Model model) {
         model.addAttribute("files", this.fileService.getFilesForUser(currentUserId));
         model.addAttribute("notes", this.noteService.getNotesForUser(currentUserId));
+        model.addAttribute("credentials", this.credentialService.getCredentialsForUser(currentUserId));
         return "home";
     }
 }
