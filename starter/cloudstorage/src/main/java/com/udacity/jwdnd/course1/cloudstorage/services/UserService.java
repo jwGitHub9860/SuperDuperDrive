70c8a165b389af966c59e05d0137bf0814dd6163
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-10 20:34:28 -0400

    refactor: Refactoring production code
    Generate Java code inside "UserSevice.java" java file to add starter code that finds user id and later use "UserSevice.java" java file to define "currentUserId" inside "HomeController.java" file
    
    Another reason is to play with new Visual Studio Code feature of automatically generating Java template code for "UserService" to see what code would be generated.

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,33 @@
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+
+@Service
+public class UserService {
+
+    private final UserMapper userMapper;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public UserService(UserMapper userMapper, BCryptPasswordEncoder passwordEncoder) {
+        this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userMapper.getUser(username) == null;
+    }
+
+    public int createUser(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        User newUser = new User(null, user.getUsername(), encodedPassword, user.getFirstName(), user.getLastName());
+        return userMapper.insert(newUser);
+    }
+
+    public User getUser(String username) {
+        return userMapper.getUser(username);
+    }
+}
