15691f6bf828762801939f5a6d6955dc0d665249
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-12 23:43:51 -0400

    refactor: Refactoring production code
    Add code that creates "File" object inside "getFiles()" method in "FilesController.java" file to later use to obtain files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FilesController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FilesController.java
@@ -46,5 +46,6 @@ public class FilesController {
 
     public String[] getFiles() {
         String filePath = System.getProperty("user.dir") + "/Uploads";
+        File directory = new File(filePath);
     }
 }
