0d1512ed202d965269a0f5ca35d9a4d6c2b16910
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-14 15:27:57 -0400

    refactor: Refactoring production code
    Add "@Options" annotation inside "UserMapper.java" file to define how "MyBatis" will handle user's primary key

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -9,4 +9,5 @@ public interface UserMapper {
     User getUser(String username);
 
     @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
+    @Options(useGeneratedKeys = true, keyProperty = "userId")
 }
