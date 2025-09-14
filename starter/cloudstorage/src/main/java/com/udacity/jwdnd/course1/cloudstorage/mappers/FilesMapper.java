c55b9a37bca6efe04ff0cfa7532d55750c343ed1
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-14 16:40:39 -0400

    refactor: Refactoring production code
    Add "@Select" annotation inside "FilesMapper.java" file to spell out user in SQL

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FilesMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FilesMapper.java
@@ -6,5 +6,5 @@ import org.apache.ibatis.annotations.*;
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
 @Mapper
-public interface FilesMapper {}
-    
+public interface FilesMapper {
+    @Select("SELECT * FROM Files WHERE fileId = #{fileId}")
