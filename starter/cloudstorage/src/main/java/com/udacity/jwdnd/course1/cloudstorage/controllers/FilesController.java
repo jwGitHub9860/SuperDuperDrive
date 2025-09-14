01d7513052efa4d693dc0d7b1c8c3bf64e3e19f9
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-13 20:24:39 -0400

    fix: A bug fix
    Replace "/files" with "/files/upload" in "@RequestMapping" annotation above "uploadFile()" method inside "FilesController.java" file to map correct path for uploaded file to go and allow file to correctly upload

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FilesController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FilesController.java
@@ -23,7 +23,7 @@ public class FilesController {
         this.userService = userService;
     }
 
-    @RequestMapping("/files")
+    @RequestMapping("/files/upload")
     public String uploadFile(@RequestParam("fileUpload") MultipartFile fileUpload) {
         // Sets up file path
         String filePath = System.getProperty("user.dir") + "/Uploads" + File.separator + fileUpload.getOriginalFilename();
