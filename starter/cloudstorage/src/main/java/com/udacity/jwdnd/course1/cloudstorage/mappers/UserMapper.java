458b6560d218f53b1e517444d1975a848f739d57
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-14 15:14:14 -0400

    refactor: Refactoring production code
    Add "@Insert" annotation inside "UserMapper.java" file to define insert statement that "MyBatis" will use

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -7,4 +7,6 @@ package com.udacity.jwdnd.course1.cloudstorage.mappers;
 public interface UserMapper {
     @Select("SELECT * FROM USERS WHERE username = #{username}")
     User getUser(String username);
+
+    @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
 }
