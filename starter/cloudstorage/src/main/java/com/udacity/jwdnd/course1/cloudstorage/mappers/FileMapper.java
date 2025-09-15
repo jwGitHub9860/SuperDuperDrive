e1c66eca96b82419ead83f1fe60c1263a50908ed
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-15 17:38:46 -0400

    style: Formatting, missing semi colons, etc; no code change
    Move "getFiles()" method above "uploadFile()" method inside "FileMapper.java" file to keep all methods with "@Select" annotation together and maintain code consistency

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FileMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/FileMapper.java
@@ -13,12 +13,12 @@ public interface FileMapper {
     @Select("SELECT * FROM Files WHERE filename = #{filename}")
     File getFileByFileName(String filename);
 
+    @Select("SELECT * FROM Files WHERE fileId = #{fileId}")
+    String[] getFiles();
+
     @Insert("INSERT INTO Files (filename, contenttype, filesize, userid, filedata) VALUES(#{filename}, #{contenttype}, #{filesize}, #{userid}, #{filedata})")
     @Options(useGeneratedKeys = true, keyProperty = "fileId")
     Integer uploadFile(File file);
-
-    @Select("SELECT * FROM Files WHERE fileId = #{fileId}")
-    String[] getFiles();
 
     @Insert("INSERT INTO Files (filename, contenttype, filesize, userid, filedata) VALUES(#{filename}, #{contenttype}, #{filesize}, #{userid}, #{filedata})")
     @Options(useGeneratedKeys = true, keyProperty = "fileId")
