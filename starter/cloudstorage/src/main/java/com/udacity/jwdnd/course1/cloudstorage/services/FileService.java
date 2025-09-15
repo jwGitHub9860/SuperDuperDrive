0f1493a0957f9e39581b7db9fd24faeb31836ad6
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-15 01:48:12 -0400

    refactor: Refactoring production code
    Copy "import statements" and "package" from "UserService.java" file and paste them into "FileService.java" file to import required classes for Java file and indicate Java file location

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -0,0 +1,6 @@
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+package com.udacity.jwdnd.course1.cloudstorage.services;
