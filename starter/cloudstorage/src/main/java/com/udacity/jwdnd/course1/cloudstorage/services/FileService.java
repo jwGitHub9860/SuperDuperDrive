151ce59b6949f4dfc196be24d204c960164c4824
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-15 01:54:30 -0400

    refactor: Refactoring production code
    Add "@Service" annotation inside "FileService.java" file to define Java file as "Service" file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/FileService.java
@@ -4,3 +4,5 @@ import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 package com.udacity.jwdnd.course1.cloudstorage.services;
+
+@Service
