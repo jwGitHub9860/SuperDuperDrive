716aedad55b3a82d212f262442a44836ad102f45
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 01:49:17 -0400

    refactor: Refactoring production code
    Add private final variables for "filename", "fileId", "contenttype", "filesize", "filedata" inside "Files.java" file to define and manage different parts of a file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
@@ -1,4 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
 public class Files {
-    
+    private final String filename;
+    private final Integer fileId;
+    private final String contenttype;
+    private final String filesize;
+    private final byte[] filedata;
