7f922db39b0a2faf4b88f6ea30802fe08ef6f3b3
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 20:37:01 -0400

    refactor: Refactoring production code
    Add "Notes" constructor inside "Notes.java" file to define "noteTitle", "noteId", "noteDescription", and "userId" as current "noteTitle", "noteId", "noteDescription", and "userId"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Notes.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Notes.java
@@ -5,4 +5,11 @@ public class Notes {
     private final Integer noteId;
     private final String noteDescription;
     private final Integer userId;
+
+    public Notes(String noteTitle, Integer noteId, String noteDescription, Integer userId) {
+        this.noteTitle = noteTitle;
+        this.noteId = noteId;
+        this.noteDescription = noteDescription;
+        this.userId = userId;
+    }
 }
