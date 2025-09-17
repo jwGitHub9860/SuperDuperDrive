c3b7d76493da097f47be51f3e683b785ab45e34e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 20:40:34 -0400

    refactor: Refactoring production code
    Add getter method that returns "noteDescription" inside "Notes.java" file to allow any method to access "noteDescription"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Notes.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Notes.java
@@ -15,4 +15,5 @@ public class Notes {
 
     public String getNoteTitle() { return noteTitle; }
     public Integer getNoteId() { return noteId; }
+    public String getNoteDescription() { return noteDescription; }
 }
