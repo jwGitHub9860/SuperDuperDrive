7d44bda9e1d126898508a858b7d3de2a1f64cd52
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 20:31:29 -0400

    refactor: Refactoring production code
    Add private final variables for "noteTitle", "noteId", "noteDescription", and "userId" inside "Notes.java" file to define and manage different parts of a note

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Notes.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Notes.java
@@ -1,3 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
-public class Notes {}
+public class Notes {
+    private final String noteTitle;
+    private final Integer noteId;
+    private final String noteDescription;
+    private final Integer userId;
+}
