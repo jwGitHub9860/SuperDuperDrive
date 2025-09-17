b29bf478d99bc8cee8076763b73018a7dcdc4caf
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 21:50:04 -0400

    refactor: Refactoring production code
    Add private final variables for "url", "credentialId", "username", "userId", "key", "password", and "decryptedPassword" inside "Credentials.java" file to define and manage different parts of a credential

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credentials.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Credentials.java
@@ -1,3 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
-public class Credentials {}
+public class Credentials {
+    private final String url;
+    private final Integer credentialId;
+    private final String username;
+    private final Integer userId;
+    private final String key;
+    private final String password;
+    private final String decryptedPassword;
+}
