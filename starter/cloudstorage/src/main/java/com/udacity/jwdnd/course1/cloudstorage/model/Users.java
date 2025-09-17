89c0079223a16512fb4718d59760a4ce8ffe925a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 23:07:27 -0400

    refactor: Refactoring production code
    Add private final variables for "userId", "username", "salt", "password", "firstName", and "lastName" inside "Users.java" file to define and manage different parts of a user

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
@@ -1,3 +1,12 @@
 package com.udacity.jwdnd.course1.cloudstorage.model;
 
-public class Users {}
+public class Users {
+    private final Integer userId;
+    private final String username;
+    private final String salt;
+    private final String password;
+    private final String firstName;
+    private final String lastName;
+
+    
+}
