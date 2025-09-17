f7798b496cff36582ffb39701c96e0567d81b18e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 21:33:46 -0400

    refactor: Refactoring production code
    Rename "contenttype" to "contentType" inside "Files.java" file to make naming consistent

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
@@ -3,27 +3,27 @@ package com.udacity.jwdnd.course1.cloudstorage.model;
 public class Files {
     private final String filename;
     private final Integer fileId;
-    private final String contenttype;
+    private final String contentType;
     private final String filesize;
     private final byte[] filedata;
 
-    public Files(String filename, Integer fileId, String contenttype, String filesize, byte[] filedata) {
+    public Files(String filename, Integer fileId, String contentType, String filesize, byte[] filedata) {
         this.filename = filename;
         this.fileId = fileId;
-        this.contenttype = contenttype;
+        this.contentType = contentType;
         this.filesize = filesize;
         this.filedata = filedata;
     }
 
     public String getFilename() { return filename; }
     public Integer getFileId() { return fileId; }
-    public String getContenttype() { return contenttype; }
+    public String getContenttype() { return contentType; }
     public String getFilesize() { return filesize; }
     public byte[] getFiledata() { return filedata; }
 
     public String setFilename() { this.filename = filename; }
     public Integer setFileId() { this.fileId = fileId; }
-    public String setContenttype() { this.contenttype = contenttype; }
+    public String setContenttype() { this.contentType = contentType; }
     public String setFilesize() { this.filesize = filesize; }
     public byte[] setFiledata() { this.filedata = filedata; }
 }
