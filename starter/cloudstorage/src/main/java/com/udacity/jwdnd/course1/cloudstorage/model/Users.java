c12e0b9618f54bf576e6609f3cef540dfa20590f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 23:30:14 -0400

    refactor: Refactoring production code
    Add setter method that sets "firstName" to current "firstName" inside "Users.java" file to define "firstName"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
@@ -28,4 +28,5 @@ public class Users {
     public final String setUsername() { this.username = username; }
     public final String setSalt() { this.salt = salt; }
     public final String setPassword() { this.password = password; }
+    public final String setFirstName() { this.firstName = firstName; }
 }
