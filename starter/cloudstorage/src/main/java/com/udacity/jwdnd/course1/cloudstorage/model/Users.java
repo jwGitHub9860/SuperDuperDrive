77b5a2600b96643ebd3940f53dc64f1999960ced
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-16 23:17:58 -0400

    refactor: Refactoring production code
    Add setter method that sets "salt" to current "salt" inside "Users.java" file to define "salt"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
@@ -26,4 +26,5 @@ public class Users {
 
     public final Integer setUserId() { this.userId = userId; }
     public final String setUsername() { this.username = username; }
+    public final String setSalt() { this.salt = salt; }
 }
