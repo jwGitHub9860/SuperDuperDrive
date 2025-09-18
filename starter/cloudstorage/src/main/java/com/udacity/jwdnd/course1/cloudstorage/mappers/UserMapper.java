f5b407c70b6cea3b766cb4c4578dcbde6ef5ec23
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-18 02:49:56 -0400

    fix: A bug fix
    Replace "model" with "Model" in first "import statement" inside "UserMapper.java" file to fix error of "com.udacity.jwdnd.course1.cloudstorage.model.Users" not existing

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -1,6 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
-import com.udacity.jwdnd.course1.cloudstorage.model;
+import com.udacity.jwdnd.course1.cloudstorage.Model;
 import org.apache.ibatis.annotations.*;
 
 @Mapper
