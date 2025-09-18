7d1121c19687dc27ec6dbe7d0c62fcc7e81ae1aa
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-17 22:04:03 -0400

    refactor: Refactoring production code
    Add private final variables for "userMapper" and "hashService" inside "AuthenticationService.java" file to allow access to methods in "UserMapper.java" and "HashService.java" files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -3,4 +3,8 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 @Service
 
 public class AuthenticationService {
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    
 }
