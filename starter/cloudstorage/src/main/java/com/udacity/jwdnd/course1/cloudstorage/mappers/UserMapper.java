41916226c5da61d69a26d5be479478a615663bcd
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-18 03:01:06 -0400

    fix: A bug fix
    Add "import statement" that imports "Users" inside "UserMapper.java" file to fix error of "User" symbol not being found

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -1,7 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.mappers;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Users;
 import org.apache.ibatis.annotations.*;
-import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
