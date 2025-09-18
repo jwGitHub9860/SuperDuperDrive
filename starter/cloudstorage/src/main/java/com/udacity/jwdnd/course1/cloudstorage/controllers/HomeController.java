0e0934e5249184db85c73214887e8f3ea1b973e7
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-18 14:51:39 -0400

    fix: A bug fix
    Add "import statement" thats imports "Authentication.java" file inside "HomeController.java" file to define "Authentication authentication" and fix error of being unable to find "Authentication" class symbol

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -3,6 +3,8 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+
 @Controller
 @RequestMapping("/home")
 public class HomeController {
