59858d0bb8e864e270061035087a7ff3fd4fa4ac
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-18 18:04:33 -0400

    fix: A bug fix
    Add "import statement" that imports "HashService.java" file inside "UserService.java" file to allow access to "HashService.java" file and fix two errors of not being able to find "HashService" class symbol

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.Users;
+import com.udacity.jwdnd.course1.cloudstorage.services.HashService;
 
 @Service
 public class UserService {
