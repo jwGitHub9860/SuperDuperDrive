8a81194ff71ddb387cf798ef402b604a572c1653
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-18 03:02:22 -0400

    fix: A bug fix
    Replace "User" with "Users" inside "UserMapper.java" file to fix error of "User" symbol not being found and obtain and return "Users" object

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/UserMapper.java
@@ -6,7 +6,7 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface UserMapper {
     @Select("SELECT * FROM USERS WHERE username = #{username}")
-    User getUser(String username);
+    Users getUser(String username);
 
     @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname) VALUES(#{username}, #{salt}, #{password}, #{firstName}, #{lastName})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
