a4c25899229f9745f163b0d13164a8095e985a7a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-19 17:16:12 -0400

    refactor: Refactoring production code
    Add "getNoteByNoteId()" method inside "NoteMapper.java" file to allow user to obtain note by note ID

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
@@ -3,4 +3,5 @@ package com.udacity.jwdnd.course1.cloudstorage.mappers;
 @Mapper
 public interface NoteMapper {
     @Select("SELECT * FROM Notes WHERE noteId = #{noteId}")
+    getNoteByNoteId(Integer noteId);
 }
