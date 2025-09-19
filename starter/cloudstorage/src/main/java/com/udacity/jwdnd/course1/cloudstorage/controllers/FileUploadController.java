a4781138f2e1fec669de4e7cd9908d1785d54420
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-19 16:52:39 -0400

    refactor: Refactoring production code
    Change "return statement" to return different string value inside "uploadFile()" method in "FileUploadController.java" file because "try-catch block" contains code that holds message stating status of file upload and to indicate where to redirect "file"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -56,7 +56,7 @@ public class FileUploadController {
         Users users = this.userService.getUser(authentication.getName()).getUserId();
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
         
-        return fileUploadStatus;
+        return "redirect:/home";
     }
 
     @RequestMapping("/files")
