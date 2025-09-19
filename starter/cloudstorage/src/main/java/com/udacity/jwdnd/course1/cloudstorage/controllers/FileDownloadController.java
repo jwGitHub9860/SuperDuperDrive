8cae8385d5a7841dbc27b3b1268a0d454529480e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-19 13:35:02 -0400

    refactor: Refactoring production code
    Add "import statement" that imports "RedirectAttributes" inside "FileDownloadController.java" file to allow "RedirectAttributes" object to be used in Java file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileDownloadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileDownloadController.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
