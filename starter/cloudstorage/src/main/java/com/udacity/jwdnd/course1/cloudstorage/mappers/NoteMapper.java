2c8a48f6026e05676f717b0b65e41186464bbf05
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-19 19:58:54 -0400

    refactor: Refactoring production code
    Add "Notes" type for "getNoteByNoteId()", "getNoteByNoteTitle()", "getNoteByUserId()", and "createNote()" methods inside "CredentialsMapper.java" file to define variable type methods will return

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
@@ -3,17 +3,17 @@ package com.udacity.jwdnd.course1.cloudstorage.mappers;
 @Mapper
 public interface NoteMapper {
     @Select("SELECT * FROM Notes WHERE noteId = #{noteId}")
-    getNoteByNoteId(Integer noteId);
+    Notes getNoteByNoteId(Integer noteId);
 
     @Select("SELECT * FROM Notes WHERE noteTitle = #{noteTitle}")
-    getNoteByNoteTitle(String noteTitle);
+    Notes getNoteByNoteTitle(String noteTitle);
 
     @Select("SELECT * FROM Notes WHERE userId = #{userId}")
-    getNoteByUserId(Integer userId);
+    Notes getNoteByUserId(Integer userId);
 
     @Insert("INSERT INTO Notes (noteTitle, noteDescription, userId) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
-    createNote(String noteTitle, String noteDescription, Integer userId);
+    Notes createNote(String noteTitle, String noteDescription, Integer userId);
 
     @Delete("DELETE FROM Notes WHERE noteId = #{noteId}")
     void deleteNoteByNoteId(Integer noteId);
