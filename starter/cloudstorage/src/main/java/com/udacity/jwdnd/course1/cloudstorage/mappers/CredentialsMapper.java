a696cd13017e9f344fd13d8a8cf398f1362153b7
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-20 19:53:33 -0400

    refactor: Refactoring production code
    Add "getCredentialByCredentialName()" method inside "CredentialsMapper.java" file to allow user to obtain credential by using "credentialName"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/CredentialsMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/CredentialsMapper.java
@@ -3,4 +3,6 @@ package com.udacity.jwdnd.course1.cloudstorage.mappers;
 @Mapper
 public interface CredentialsMapper {
     Credentials getCredentialByCredentialId(Integer credentialId);
+
+    Credentials getCredentialByCredentialName(String credentialName);
 }
