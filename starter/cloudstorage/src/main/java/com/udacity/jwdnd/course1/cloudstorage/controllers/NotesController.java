32e9e2f371c0438141d55a827d24456c64e1732c
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-19 23:41:41 -0400

    refactor: Refactoring production code
    Add four "import statements" that import "FileService.java", "NoteService.java", "CredentialService.java", and "UserService.java" files inside "NotesController.java" file to allow access to methods in Java files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NotesController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NotesController.java
@@ -1,4 +1,9 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
+import com.udacity.jwdnd.course1.cloudstorage.services.CredentialService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+
 @Controller
 public class NoteController {}
