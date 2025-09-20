01772d75b29e7bcc02a09b5411418562707d9ddf
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-19 22:10:54 -0400

    refactor: Refactoring production code
    Add "RedirectAttributes redirectAttributes" as input for "deleteFile()" method inside "FileService.java" file to later be able to store message stating if file has successful delete

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -60,7 +60,7 @@ public class FileUploadController {
     }
 
     @RequestMapping("/files")
-    public void deleteFile(@PathVariable String filename, Authentication authentication) {
+    public void deleteFile(@PathVariable String filename, Authentication authentication, RedirectAttributes redirectAttributes) {
         String filePath = System.getProperty("user.dir") + "/Uploads" + File.separator + filename;
         File file = new File(filePath);
         if (file.delete()) {
