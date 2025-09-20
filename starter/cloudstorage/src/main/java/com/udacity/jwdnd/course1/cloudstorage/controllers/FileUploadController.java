3f010ba7b81d46c9f2965f156e54a93d4d94caee
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-19 22:40:38 -0400

    refactor: Refactoring production code
    Add code that adds "files" as attribute inside "FileService.java" file to add "files" attribute to all models that are defined in controller class

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -70,5 +70,6 @@ public class FileUploadController {
         }
 
         Users users = this.userService.getUser(authentication.getName()).getUserId();
+        model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
     }
 }
