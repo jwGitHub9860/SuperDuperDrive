1245d44e7b137e51f0f6f0157cee41745d0f7062
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-20 20:43:51 -0400

    refactor: Refactoring production code
    Add "@Select" annotation inside "CredentialsMapper.java" file to spell out user in SQL for "getCredentialByCredentialName()" method

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/CredentialsMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/CredentialsMapper.java
@@ -5,6 +5,7 @@ public interface CredentialsMapper {
     @Select("SELECT * FROM CREDENTIALS WHERE credentialid = #{credentialId}")
     Credentials getCredentialByCredentialId(Integer credentialId);
 
+    @Select("SELECT * FROM Credentials WHERE credentialName = #{credentialName}")
     Credentials getCredentialByCredentialName(String credentialName);
 
     Credentials getCredentialByUserId(Integer userId);
