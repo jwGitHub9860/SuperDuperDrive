feb8a52bf775fd23095753c4010ec520ca45d922
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-21 00:20:17 -0400

    refactor: Refactoring production code
    Add "import statement" that imports "Files.java" file inside "CredentialsMapper.java" file to allow access to Java file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Files;
 import com.udacity.jwdnd.course1.cloudstorage.model.Users;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
