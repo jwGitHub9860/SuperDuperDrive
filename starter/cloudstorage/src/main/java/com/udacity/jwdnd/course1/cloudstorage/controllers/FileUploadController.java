aa37a5a5f0f1fc459304d4fbbb9a511766c15d30
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-21 23:45:47 -0400

    refactor: Refactoring production code
    Add code that creates new "ArrayList" object for "uploadedFiles" inside "NotesController.java" file to define "uploadedFiles" as empty array list

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -30,7 +30,7 @@ public class FileUploadController {
     private final NoteService noteService;
     private final CredentialService credentialService;
     private final UserService userService;
-    private List<Files> uploadedFiles;
+    private List<Files> uploadedFiles = new ArrayList<Files>();
 
     public FileUploadController(FileService fileService, NoteService noteService, CredentialService credentialService, UserService userService) {
         this.fileService = fileService;
