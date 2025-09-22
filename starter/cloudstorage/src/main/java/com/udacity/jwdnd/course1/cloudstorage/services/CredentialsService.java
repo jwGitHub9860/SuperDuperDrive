d691f79b0229f3ed62bee2fe986c21c963193913
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-22 17:40:25 -0400

    refactor: Refactoring production code
    Add private final variable for "CredentialsMapper.java" inside "CredentialsService.java" file to use it to access methods inside "CredentialsMapper.java"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
@@ -3,4 +3,6 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 import com.udacity.jwdnd.course1.cloudstorage.mappers.CredentialMapper;
 
 @Service
-public class CredentialService {}
+public class CredentialService {
+    private final CredentialsMapper credentialsMapper;
+}
