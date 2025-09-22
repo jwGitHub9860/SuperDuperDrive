eb485acc55517bd8fcd6595ef14236b073a48fed
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-21 21:24:59 -0400

    refactor: Refactoring production code
    Add "import statement" that imports "Model" inside "FileDownloadController.java", "FileUploadController.java", and "HomeController.java" files to be able to use "Model" type variables in Java files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileDownloadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileDownloadController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpSecurity;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
