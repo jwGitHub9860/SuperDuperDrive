a986e034115624a3eaab02237181f23d9d73eeb6
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-22 17:53:02 -0400

    fix: A bug fix
    Replace "CredentialService" with "CredentialsService" inside "CredentialsService.java" file to correct error of naming public class with incorrect name

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
@@ -3,7 +3,7 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 import com.udacity.jwdnd.course1.cloudstorage.mappers.CredentialsMapper;
 
 @Service
-public class CredentialService {
+public class CredentialsService {
     private final CredentialsMapper credentialsMapper;
 
     public CredentialsMapper(CredentialsMapper credentialsMapper) {
