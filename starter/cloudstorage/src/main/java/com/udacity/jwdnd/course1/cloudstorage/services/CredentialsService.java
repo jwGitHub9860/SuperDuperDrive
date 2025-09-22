6462b2833e9b4987d40f3fde3aae4728271cd341
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-22 17:41:12 -0400

    refactor: Refactoring production code
    Change "CredentialMapper" to "CredentialsMapper" inside "CredentialsService.java" file to correctly import "CredentialsMapper.java"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
@@ -1,6 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage.services;
 
-import com.udacity.jwdnd.course1.cloudstorage.mappers.CredentialMapper;
+import com.udacity.jwdnd.course1.cloudstorage.mappers.CredentialsMapper;
 
 @Service
 public class CredentialService {
