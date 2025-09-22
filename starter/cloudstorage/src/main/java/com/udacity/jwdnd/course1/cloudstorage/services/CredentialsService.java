75cd39ee03e926143822f1d362d95e3a867f19e6
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-22 17:46:35 -0400

    refactor: Refactoring production code
    Add constructor inside "CredentialsService.java" file to hold code for defining private final variables

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialsService.java
@@ -5,4 +5,6 @@ import com.udacity.jwdnd.course1.cloudstorage.mappers.CredentialsMapper;
 @Service
 public class CredentialService {
     private final CredentialsMapper credentialsMapper;
+
+    public CredentialsMapper(CredentialsMapper credentialsMapper) {}
 }
