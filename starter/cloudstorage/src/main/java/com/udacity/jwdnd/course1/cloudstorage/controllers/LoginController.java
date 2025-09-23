da0af36558f2f46ea97574aa1d20b88400509a8a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-22 23:44:44 -0400

    refactor: Refactoring production code
    Add "Controller" annotation inside "LoginController.java" file to define file as controller file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -4,3 +4,5 @@ import com.udacity.jwdnd.course1.cloudstorage.controllers.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.controllers.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.controllers.CredentialsService;
 import com.udacity.jwdnd.course1.cloudstorage.controllers.UserService;
+
+@Controller
