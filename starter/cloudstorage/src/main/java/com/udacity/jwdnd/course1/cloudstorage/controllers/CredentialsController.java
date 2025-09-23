2852222ce9e9b5d3aee5371d69b94e4e5511f540
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-23 02:57:40 -0400

    refactor: Refactoring production code
    Add "import statement" that imports "UserService.java" inside "LoginController.java" file to allow access to methods in "UserService.java"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -3,3 +3,4 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
 import com.udacity.jwdnd.course1.cloudstorage.services.CredentialsService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
