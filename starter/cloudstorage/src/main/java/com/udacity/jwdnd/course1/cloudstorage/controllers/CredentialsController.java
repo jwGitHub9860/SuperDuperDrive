29cca3bf5adc777ddc085e770d7e064c4cfc0786
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-23 14:57:44 -0400

    refactor: Refactoring production code
    Add code that defines "fileService", "noteService", "credentialService", and "userService" as current "fileService", "noteService", "credentialService", and "userService" inside constructor in "CredentialsController.java" file to be able to call methods in respective files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -12,5 +12,10 @@ public class CredentialsController {
     private final CredentialsService credentialsService;
     private final UserService userService;
 
-    public CredentialsController(FileService fileService, NoteService noteService, CredentialsService credentialsService, UserService userService) {}
+    public CredentialsController(FileService fileService, NoteService noteService, CredentialsService credentialsService, UserService userService) {
+        this.fileService = fileService;
+        this.noteService = noteService;
+        this.credentialsService = credentialsService;
+        this.userService = userService;
+    }
 }
