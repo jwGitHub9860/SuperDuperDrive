d0ebe23c037cd417130033ef09221f89bd480cca
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-22 22:39:07 -0400

    refactor: Refactoring production code
    Add "Model model" for input of "deleteFile()" method inside "FileUploadController.java" file to be able to add "files", "notes", and "credentials" as attributes to webpage

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -72,7 +72,7 @@ public class FileUploadController {
     }
 
     @RequestMapping("/files")
-    public void deleteFile(@PathVariable String filename, Authentication authentication, RedirectAttributes redirectAttributes) {
+    public void deleteFile(@PathVariable String filename, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         String filePath = System.getProperty("user.dir") + "/Uploads" + File.separator + filename;
         File file = new File(filePath);
         if (file.delete()) {
