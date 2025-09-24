ee112d44382a8424a9f1786aa15e2a06644225ce
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-24 18:46:32 -0400

    refactor: Refactoring production code
    Add import statement that imports "MultipartFile" package inside "CredentialsController.java" file to allow "MultipartFile" variables to be used inside Java file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
