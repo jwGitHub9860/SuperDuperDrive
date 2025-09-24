512afc64350f370687999e26f2e2442dbe9f58eb
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-23 23:28:40 -0400

    refactor: Refactoring production code
    Add code that adds "files" as attribute to "Model" inside "getLoginPage()" method in "LoginController.java" file to display login page when program runs

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -28,5 +28,6 @@ public class LoginController {
     @RequestMapping("/login")
     public String getLoginPage(@RequestParam("userCredentials") MultipartFile loginCredentials, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         Users users = userService.getUser(authentication.getName());
+        model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
     }
 }
