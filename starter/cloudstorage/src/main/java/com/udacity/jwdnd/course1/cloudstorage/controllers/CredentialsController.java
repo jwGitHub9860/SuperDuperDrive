33a2c5ccde785b49695e69a270abd013094f140f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-24 20:45:07 -0400

    refactor: Refactoring production code
    Add code that calls "createCredential()" method from "CredentialsService.java" inside "addCredentials()" method in "CredentialsController.java" file to add "webpageCredentials" to database

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -26,6 +26,8 @@ public class CredentialsController {
     }
 
     public String addCredentials(@RequestParam("chooseCredentials") MultipartFile webpageCredentials, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+        credentialsService.createCredential(webpageCredentials.getUrl(), webpageCredentials.getUsername(), webpageCredentials.getKey(), webpageCredentials.getPassword(), webpageCredentials.getCredentialId());
+        
         Users users = userService.getUser(authentication.getName());
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
         model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
