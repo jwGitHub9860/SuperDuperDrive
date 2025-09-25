ecbca1f65b44e9a81c7da8262b0bc3f77b85eeb3
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-25 14:31:25 -0400

    refactor: Refactoring production code
    Add code that adds "credentials" as attribute to "Model" inside "deleteCredential()" method in "CredentialsController.java" file to allow user to interact with "credentials" options on webpage

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -55,5 +55,6 @@ public class CredentialsController {
         Users users = userService.getUser(authentication.getName());
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
         model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
+        model.addAttribute("credentials", this.credentialsService.getCredentialByUserId(users.getUserId()));
     }
 }
