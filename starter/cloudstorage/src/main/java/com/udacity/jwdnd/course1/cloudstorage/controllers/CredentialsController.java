415e3c46b7b0badadacb93dc1aeaaef72f7f87c9
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-24 20:46:20 -0400

    refactor: Refactoring production code
    Add "return statement" that returns "redirect:/home" inside "addCredentials()" method in "CredentialsController.java" file to take user back to home page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -33,5 +33,7 @@ public class CredentialsController {
         model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
         model.addAttribute("credentials", this.credentialsService.getCredentialByUserId(users.getUserId()));
         redirectAttributes.addFlashAttribute("credentials_status", "Credentials added successfully!");
+
+        return "redirect:/home";
     }
 }
