fbf1fb3afce2394f1f5ab0f6f06ced9f558b669c
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-26 11:01:48 -0400

    refactor: Refactoring production code
    Rename "getContenttype()" method to "getContentType()" inside "Files.java" file to improve readability

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
@@ -19,7 +19,7 @@ public class Files {
 
     public final String getFilename() { return filename; }
     public final Integer getFileId() { return fileId; }
-    public final String getContenttype() { return contentType; }
+    public final String getContentType() { return contentType; }
     public final String getFilesize() { return filesize; }
     public final byte[] getFiledata() { return filedata; }
     public final Integer getUserId() { return userId; }
