1b76914f4ee7ba8b14d191bc53ec4e53be9909db
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-25 20:58:01 -0400

    fix: A bug fix
    Change "return statement" with code that calls "deleteNoteByNoteId()" from "NoteMapper.java" inside "deleteNoteByNoteId()" method in "NoteService.java" file to fix error of void methods not being able to return value

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteService.java
@@ -32,6 +32,6 @@ public class NoteService {
     }
 
     public void deleteNoteByNoteId(Integer noteId) {
-        return noteMapper.deleteNoteByNoteId(noteId);
+        noteMapper.deleteNoteByNoteId(noteId);
     }
 }
