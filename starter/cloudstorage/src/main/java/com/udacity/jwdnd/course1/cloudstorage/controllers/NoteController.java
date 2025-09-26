a2be6cda230e29056052aa85f29d255cb36a8b96
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-26 19:44:20 -0400

    fix: A bug fix
    Replace ".getOriginalFileName()" with ".getName()" in "Notes" constructor call inside "addNote()" method in "NoteController.java" file to fix error of "getOriginalFileName()" being undefined for "MultipartFile" type

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -29,7 +29,7 @@ public class NoteController {
     }
 
     public String addNote(@RequestParam("addNote") MultipartFile createNote, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
-        Notes newNote = new Notes(createNote.getOriginalFileName(), null, createNote.getContentType(), createNote.getUserId());
+        Notes newNote = new Notes(createNote.getName(), null, createNote.getContentType(), createNote.getUserId());
 
         Users users = userService.getUser(authentication.getName());
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
