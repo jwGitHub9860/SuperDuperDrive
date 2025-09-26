a2364f56d46bf9440dc17bc19c89680b61177379
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-25 21:53:36 -0400

    fix: A bug fix
    Change setters to "void" methods inside "Files.java" file to fix error of setters not returning their respective result types when setters are suppose to only set their respective values

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Files.java
@@ -24,10 +24,10 @@ public class Files {
     public final byte[] getFiledata() { return filedata; }
     public final Integer getUserId() { return userId; }
 
-    public final String setFilename(String filename) { this.filename = filename; }
-    public final Integer setFileId(Integer fileId) { this.fileId = fileId; }
-    public final String setContenttype(String contentType) { this.contentType = contentType; }
-    public final String setFilesize(String filesize) { this.filesize = filesize; }
-    public final byte[] setFiledata(byte[] filedata) { this.filedata = filedata; }
-    public final Integer setUserId(Integer userId) { this.userId = userId; }
+    public final void setFilename(String filename) { this.filename = filename; }
+    public final void setFileId(Integer fileId) { this.fileId = fileId; }
+    public final void setContenttype(String contentType) { this.contentType = contentType; }
+    public final void setFilesize(String filesize) { this.filesize = filesize; }
+    public final void setFiledata(byte[] filedata) { this.filedata = filedata; }
+    public final void setUserId(Integer userId) { this.userId = userId; }
 }
