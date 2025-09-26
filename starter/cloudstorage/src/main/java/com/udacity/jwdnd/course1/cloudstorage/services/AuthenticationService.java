71b944102b5f60806d982f9685787a3b3c35866a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-26 18:52:27 -0400

    refactor: Refactoring production code
    Delete "import statement" that imports "org.springframework.security.core.userdetails.User" package inside "AuthenticationService.java" file to remove unused "import statement"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -6,7 +6,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
 
 import com.udacity.jwdnd.course1.cloudstorage.mappers.UserMapper;
