2dc7d5be158d78232aff3bbbb18e9258589ac3cd
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-26 17:20:34 -0400

    refactor: Refactoring production code
    Add flash attribute to "RedirectAttributes" inside "editNote()" method in "NoteController.java" file to store message indicating that editing note is successful

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -44,5 +44,6 @@ public class NoteController {
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
         model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
         model.addAttribute("credentials", this.credentialsService.getCredentialByUserId(users.getUserId()));
+        redirectAttributes.addFlashAttribute("edit_note_status", "Note edited successfully!");
     }
 }
