e4b517c9a467827d9983efcea7c9f5f63a9e6653
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-27 00:32:15 -0400

    fix: A bug fix
    Add "import statement" that imports "Authentication" inside "HomeController.java" file to fix error of "Authentication" not being resolved to a type

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
