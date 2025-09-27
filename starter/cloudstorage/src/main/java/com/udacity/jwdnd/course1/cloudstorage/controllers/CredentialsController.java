f6d59e4980d4e04f357a83594482a0da7732e0b9
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-26 21:20:40 -0400

    feat: A new feature
    Add "import statement" that imports "GetMapping" annotation inside "CredentialsController.java" file to allow "@GetMapping" annotation to be used inside Java file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -2,6 +2,7 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
