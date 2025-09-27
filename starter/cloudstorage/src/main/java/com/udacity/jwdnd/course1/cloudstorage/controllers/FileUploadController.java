aebc06219643f4a8d8afdde7e57aff90f3eeeca5
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-27 12:17:51 -0400

    feat: A new feature
    Add code that adds "notes" as attribute to "Model" inside "deleteFile()" method in "FileUploadController.java" file to allow user to interact with "notes" options on webpage

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -84,5 +84,6 @@ public class FileUploadController {
 
         Users users = userService.getUser(authentication.getName());
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
+        model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
     }
 }
