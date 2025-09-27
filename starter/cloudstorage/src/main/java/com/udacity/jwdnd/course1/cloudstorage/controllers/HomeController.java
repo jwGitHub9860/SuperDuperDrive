7ff7480626264eb3ac47dd92847847df21f69df9
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-27 09:57:21 -0400

    chore: Updating build tasks, package manager configs, etc; no production code change
    Delete "import statement" that imports "AuthenticationProvider" inside "HomeController.java" file because it is not used in Java file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,6 +1,5 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
