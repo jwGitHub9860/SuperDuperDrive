0ec0bce2f46ac392e635cdcca4df8abf51209969
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-26 22:29:52 -0400

    feat: A new feature
    Add "notes" as attribute to "Model" inside "LoginController.java" file to allow user to interact with "notes" options on webpage

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -30,6 +30,7 @@ public class LoginController {
     public String getLoginPage(@RequestParam("userCredentials") MultipartFile loginCredentials, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         Users users = userService.getUser(authentication.getName());
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
+        model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
         redirectAttributes.addFlashAttribute("displayLoginPage", true);
     }
 }
