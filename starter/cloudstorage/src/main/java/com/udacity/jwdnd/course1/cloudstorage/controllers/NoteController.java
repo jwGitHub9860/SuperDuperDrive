9c291cf26b20ed4e2503a142c4b630c72f67d8f1
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-28 21:25:17 -0400

    fix: A bug fix
    Replace "chosenNote.setFilename(noteService.getNoteTitle())" with "chosenNoteTitle" inside "editNote()" method in "NoteController.java" file to fix errors of "chosenNote" being undefined and "getNoteTitle()" method being undefined for "NoteService" type

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -43,7 +43,7 @@ public class NoteController {
 
     public String editNote(@RequestParam("chosenNote") String chosenNoteTitle, @RequestParam("chosenNote") Integer chosenNoteId, @RequestParam("chosenNote") String chosenNoteDescription, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         Users users = userService.getUser(authentication.getName());
-        noteService.editNote(chosenNote.setFilename(noteService.getNoteTitle()), chosenNote.getNoteDescription(), users.getUserId());
+        noteService.editNote(chosenNoteTitle, chosenNote.getNoteDescription(), users.getUserId());
         
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
         model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
