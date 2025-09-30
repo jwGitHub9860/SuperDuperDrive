574161934bcc1466b65586d94d67f4fa8f895426
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-30 14:56:03 -0400

    fix: A bug fix
    Replace "@RequestParam("chooseCredential") MultipartFile chosenCredential" with "@RequestParam("chosenCredentials") String chosenCredentialsUrl, @RequestParam("chosenCredentials") String chosenCredentialsUsername, @RequestParam("chosenCredentials") String chosenCredentialsKey, @RequestParam("chosenCredentials") String chosenCredentialsPassword, @RequestParam("chosenCredentials") Integer chosenCredentialsId" as input for "addCredentials()" method inside "CredentialsController.java" file to fix errors of "getUrl()", "getUsername()", "getKey()", "getPassword()", and "getCredentialId()" methods being undefined for "MultipartFile" type

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -46,9 +46,9 @@ public class CredentialsController {
     }
 
     @GetMapping
-    public String editCredentials(@RequestParam("chooseCredential") MultipartFile chosenCredential, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+    public String editCredentials(@RequestParam("chosenCredentials") String chosenCredentialsUrl, @RequestParam("chosenCredentials") String chosenCredentialsUsername, @RequestParam("chosenCredentials") String chosenCredentialsKey, @RequestParam("chosenCredentials") String chosenCredentialsPassword, @RequestParam("chosenCredentials") Integer chosenCredentialsId, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         credentialsService.editCredentials(chosenCredential.getUrl(), chosenCredential.getUsername(), chosenCredential.getKey(), chosenCredential.getPassword(), chosenCredential.getCredentialId());
-        
+
         Users users = userService.getUser(authentication.getName());
         model.addAttribute("files", this.fileService.getFileByUserId(users.getUserId()));
         model.addAttribute("notes", this.noteService.getNoteByUserId(users.getUserId()));
