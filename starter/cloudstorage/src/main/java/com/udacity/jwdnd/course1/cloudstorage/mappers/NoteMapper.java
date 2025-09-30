6db0eacbf6c87025cbbdad4232e7f842291742f8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-30 14:05:06 -0400

    fix: A bug fix
    Replace "noteId" with "noteTitle" in "@Delete" annotation and "deleteNoteByNoteId(Integer noteId)" method name with "deleteNoteByNoteTitle(String noteTitle)" inside "NoteMapper.java" file to fix error of "deleteNoteByNoteTitle()" method having no definition in "NoteMapper.java" file and allow user to delete note by note title

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
@@ -22,6 +22,6 @@ public interface NoteMapper {
     @Update("UPDATE Notes SET noteTitle = #{noteTitle}, noteDescription = #{noteDescription} WHERE noteId = #{noteId}")
     void editNote(String noteTitle, String noteDescription, Integer noteId);
 
-    @Delete("DELETE FROM Notes WHERE noteId = #{noteId}")
-    void deleteNoteByNoteId(Integer noteId);
+    @Delete("DELETE FROM Notes WHERE noteTitle = #{noteTitle}")
+    void deleteNoteByNoteTitle(String noteTitle);
 }
