9ff80ed4e7717618e990833fa52afbb992107956
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-30 11:14:41 -0400

    feat: A new feature
    Replace "_blank" with "_parent" inside "home.html" file to open linked document of "View" option in initial webpage.
    
    This is done to see if link will open in parent frameset and produce expected result.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
