d9a78f5b96ef57e6d8de3b6f0325f9bf5a2e2c66
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-30 14:17:17 -0400

    refactor: Refactoring production code
    Remove "import statements" that import "MultipartFile" and "Notes.java" inside "NoteController.java" file to remove unused "import statements" in Java file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -4,10 +4,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
 import com.udacity.jwdnd.course1.cloudstorage.model.Users;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
