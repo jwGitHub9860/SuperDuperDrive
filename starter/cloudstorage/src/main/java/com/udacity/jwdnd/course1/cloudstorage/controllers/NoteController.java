27d8d2134bcdbb86569f19fed485cf769822d3b9
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-01 19:01:13 -0400

    refactor: Refactoring production code
    Add "import statement" that imports "List" inside "NoteController.java" and "CredentialsController.java" files to allow "List" variable to be used and created inside Java files

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -1,5 +1,7 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import java.util.List;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
