597b8f055b3fe60fdd6a4eda30891c4001060738
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-09-30 22:19:22 -0400

    docs: Changes to documentation
    Add comment above "@Scope" annotation inside "Users.java" file to explain purpose of annotation

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/model/Users.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.context.annotation.Scope;
 
+// Create New Bean Instance for each User Session in Web Application
 @Scope("session")
 public class Users {
     private final Integer userId;
