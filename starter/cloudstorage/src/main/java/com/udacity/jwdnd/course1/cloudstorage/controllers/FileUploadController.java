924dd57628790c2a8e345031e6bdcf8254e791fc
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-02 19:06:28 -0400

    fix: Correct path of "@PostMapping" annotation value for "home.html" connection
    Change path of "@PostMapping" annotation for "uploadFile()" method inside "FileUploadController.java" file to fix mistake of "home.html" being unable to access "uploadFile()" method in "FileUploadController.java" file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileUploadController.java
@@ -33,7 +33,7 @@ public class FileUploadController {
         this.userService = userService;
     }
 
-    @PostMapping("/upload")
+    @PostMapping("/uploadedFile")
     public String uploadFile(@RequestParam("fileUpload") MultipartFile fileUpload, Model model, Authentication authentication, RedirectAttributes redirectAttributes) throws IOException {
         Users users = userService.getUser(authentication.getName());
         Files chosenFile = new Files(fileUpload.getOriginalFilename(), null, fileUpload.getContentType(), Long.toString(fileUpload.getSize()), fileUpload.getBytes(), users.getUserId());
