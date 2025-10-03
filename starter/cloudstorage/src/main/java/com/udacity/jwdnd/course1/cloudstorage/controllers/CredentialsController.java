6723af79a7ecc10468ba8febb5330e91bbc918d5
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-03 14:45:32 -0400

    fix: Fix undefined "newCredentials"
    Add "Credentials" constructor call inside "addCredentials()" method in "CredentialsController.java" file to fix error of "newCredentials" having no definition

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -37,6 +37,7 @@ public class CredentialsController {
     @GetMapping
     public String addCredentials(@RequestParam("addCredentials") String addCredentialsUrl, @RequestParam("addCredentials") Integer addCredentialsId, @RequestParam("addCredentials") String addCredentialsUsername, @RequestParam("addCredentials") String addCredentialsKey, @RequestParam("addCredentials") String addCredentialsPassword, @RequestParam("addCredentials") String addCredentialsDecryptedPassword, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         Users users = userService.getUser(authentication.getName());
+        Credentials newCredentials = new Credentials(addCredentialsUrl, addCredentialsId, addCredentialsUsername, addCredentialsId, addCredentialsKey, addCredentialsPassword, addCredentialsDecryptedPassword);
 
         // Checks if New Credentials are Duplicate
         try {
