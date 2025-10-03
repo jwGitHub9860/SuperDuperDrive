5a60fc41e9dcb7d3e0a968cb7b1a039b175a1b3a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-02 21:40:23 -0400

    fix: Add "Credentials.java" import statement to fix bug
    Add "import statement" that imports "Credentials.java" inside "CredentialsController.java" file to fix error of "Credentials.java" file having no definition

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Credentials;
 import com.udacity.jwdnd.course1.cloudstorage.model.Users;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
