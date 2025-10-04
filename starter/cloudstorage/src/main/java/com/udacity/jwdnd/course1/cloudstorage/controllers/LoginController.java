5cad7c16776bbd7e959291d5d1b7cc77ebf2c8d0
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-04 02:23:08 -0400

    fix: Fix access to Login Page
    Remove all code that accesses and uses files, notes, and credentials inside "LoginController.java" file to prevent files, notes, and credentials from accessing login page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -1,39 +1,13 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
-import com.udacity.jwdnd.course1.cloudstorage.services.NoteService;
-import com.udacity.jwdnd.course1.cloudstorage.services.CredentialsService;
-import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
-import com.udacity.jwdnd.course1.cloudstorage.model.Users;
 
 @Controller
 public class LoginController {
-    private final FileService fileService;
-    private final NoteService noteService;
-    private final CredentialsService credentialsService;
-    private final UserService userService;
-
-    public LoginController(FileService fileService, NoteService noteService, CredentialsService credentialsService, UserService userService) {
-        this.fileService = fileService;
-        this.noteService = noteService;
-        this.credentialsService = credentialsService;
-        this.userService = userService;
-    }
-
     @RequestMapping("/login")
-    public String getLoginPage(@RequestParam("userCredentials") MultipartFile loginCredentials, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
-        Users users = userService.getUser(authentication.getName());
-        model.addAttribute("files", this.fileService.getAllFilesByUserId(users.getUserId()));
-        model.addAttribute("notes", this.noteService.getAllNotesByUserId(users.getUserId()));
-        model.addAttribute("credentials", this.credentialsService.getAllCredentialsByUserId(users.getUserId()));
-
+    public String getLoginPage(RedirectAttributes redirectAttributes) {
         // Creates Connection between "getLoginPage()" Method & code that Displays Login Page Status inside "home.html" file
         redirectAttributes.addFlashAttribute("displayLoginPage", true);
 
