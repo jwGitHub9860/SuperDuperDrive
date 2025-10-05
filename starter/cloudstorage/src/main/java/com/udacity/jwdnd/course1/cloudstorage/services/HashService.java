11eff9c1eb7b5c91013f7d9bc2a9cb8473948ca8
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-05 15:52:11 -0400

    fix: Replace "@Service" with "@Component"
    Replace "@Service" annotation with "@Component" inside "HashService.java" file to detect custom beans automatically, inject specified dependencies into application, and prevent errors in Java file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/HashService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/HashService.java
@@ -3,7 +3,6 @@ package com.udacity.jwdnd.course1.cloudstorage.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
@@ -11,7 +10,8 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.util.Base64;
 
-@Service
+// Detects Custom Beans Automatically & Injects Specified Dependencies into Application
+@Component
 public class HashService {
 
     public final Logger logger = LoggerFactory.getLogger(HashService.class);
