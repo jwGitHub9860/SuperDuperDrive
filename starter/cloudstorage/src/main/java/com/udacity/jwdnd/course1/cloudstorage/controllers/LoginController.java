88369d0a85851acc02cf30bba38278d7390a9d4a
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-06 16:20:57 -0400

    fix: Remove all code that uses "RedirectAttributes"
    Remove all code that uses "RedirectAttributes" inside "LoginController.java" file because "login.html" file already has text and information for login page

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -2,17 +2,13 @@ package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-    
-    @GetMapping
-    public String getLoginPage(RedirectAttributes redirectAttributes) {
-        // Creates Connection between "getLoginPage()" Method & code that Displays Login Page Status inside "home.html" file
-        redirectAttributes.addFlashAttribute("displayLoginPage", true);
 
+    @GetMapping
+    public String getLoginPage() {
         return "login";
     }
 }
