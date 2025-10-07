35ec63fc72f4d6c523dacc48ec454456c61a0d14
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-06 20:11:26 -0400

    refactor: Remove unused variables and "import statements"
    Remove unused variables and "import statements" inside "FileDownloadController.java" file to decrease code amount in file

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileDownloadController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/FileDownloadController.java
@@ -1,29 +1,16 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.net.URLConnection;
-import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Files;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
-
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class FileDownloadController {
@@ -43,8 +30,6 @@ public class FileDownloadController {
 
         // Checks if Chosen File Exists
         if (chosenFile.exists()) {
-            InputStreamResource resource = new InputStreamResource(new FileInputStream(chosenFile));
-            
             // Obtains "chosenFile" Media Type (MIME type) for Content Type of "chosenFile"
             String mimeType = URLConnection.guessContentTypeFromName(chosenFile.getName());
 
