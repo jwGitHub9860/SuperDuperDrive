d665ea2032c3c78594189d4971452305743809a3
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-07 22:28:04 -0400

    feat: Add new "signUp()" method to allow user to sign up on webpage
    Add "signUp()" method inside "SignupController.java" file to allow user to sign up for new account on webpage

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignupController.java
@@ -1,5 +1,10 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/signup")
 public class SignupController {
-
+    
     @GetMapping
     public String getSignUpPage() {
         return "signup";
+    }
+
+    @Bean
+    public UserDetailsService signUp() {
+        UserDetails userDetails = User.withUsername("user")
+                                        .password("password")
+                                        .roles("USER")
+                                        .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
