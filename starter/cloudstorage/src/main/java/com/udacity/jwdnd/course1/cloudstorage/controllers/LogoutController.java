9d8f10cdf4ee1c41e4695e6aef93e7b02d75f841
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-08 16:43:24 -0400

    refactor: Remove unused "import statements"
    Remove unused "import statements" inside "LoginController.java" file to decrease project code and improve performance

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LogoutController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LogoutController.java
@@ -1,18 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
-import java.io.IOException;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
