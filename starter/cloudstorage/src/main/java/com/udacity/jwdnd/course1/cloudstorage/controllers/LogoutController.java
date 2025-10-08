fcc28d9259ec88112ecf52598f5a6a814d36ae6d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-08 16:41:14 -0400

    fix: Change "getLogoutPage()" method to fix mistake of not taking user back to logout page
    Add new "RedirectAttributes" variable inside "getLogoutPage()" method in "LoginController.java" file to indicate that user is at logout page on webpage

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LogoutController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LogoutController.java
@@ -18,13 +18,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/logout")
-public class LogoutController implements LogoutSuccessHandler {
+public class LogoutController {
 
     @GetMapping
     public String getLogoutPage(RedirectAttributes redirectAttributes) {
         // Creates Connection between "getLogoutPage()" Method & code that Displays Logout Page Status inside "login.html" file
         redirectAttributes.addFlashAttribute("log_out_success_msg", true);
-        
-        return "logout";
+
+        // Take User Back to Logout Page
+        return "redirect:/logout";
     }
 }
