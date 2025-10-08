9ef1281b6cf016935022432dcab1ce1aac9a2a96
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-08 14:54:08 -0400

    feat: Add new logout method to implement "LogoutSuccessHandler"
    Add new logout method inside "LogoutController.java" file to implement "LogoutSuccessHandler"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LogoutController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LogoutController.java
@@ -1,15 +1,37 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/logout")
-public class LogoutController {
+
+// Detects Custom Beans Automatically & Injects Specified Dependencies into Application
+@Component("myLogoutSuccessHandler")
+public class LogoutController implements LogoutSuccessHandler {
 
     @GetMapping
     public String getLogoutPage() {
         return "logout";
+    }
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+        if (session != null) {
+            session.removeAttribute("user");
+        }
     }
 }
