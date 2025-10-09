a501c2e9492d53a9cdf53d8ecec9e7103c70ba56
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-09 00:43:52 -0400

    refactor: Remove "try-catch block" to prevent program from stopping while running
    Remove "try-catch block" inside "addCredentials()" method in "CredentialsController.java" file to prevent program from stopping or crashing while running & decrease project code

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/CredentialsController.java
@@ -57,23 +57,17 @@ public class CredentialsController {
         Credentials newCredentials = new Credentials(addCredentialsUrl, addCredentialsId, addCredentialsUsername, users.getUserId(), encodedKey, encryptedCredentialsPassword);
 
         // Checks if New Credentials are Duplicate
-        try {
-            for(Credentials credentialsItem : allCredentials) {
-                if (credentialsItem.getUsername() == addCredentialsUsername) {
-                    throw new IllegalArgumentException("Credentials are duplicate!");
-                }
+        for(Credentials credentialsItem : allCredentials) {
+            if (credentialsItem.getUsername() == addCredentialsUsername) {
+                // Creates Connection between "addCredentials()" Method & code that Displays Status of Duplicate Credentials inside "home.html" file
+                redirectAttributes.addFlashAttribute("add_credentials_not_duplicate", false);
             }
-            credentialsService.createCredentials(addCredentialsUrl, addCredentialsUsername, encodedKey, encryptedCredentialsPassword, users.getUserId());
-            allCredentials.add(newCredentials);
-
-            // Creates Connection between "addCredentials()" Method & code that Displays Status of Adding Credentials Successfully inside "home.html" file
-            redirectAttributes.addFlashAttribute("add_credentials_not_duplicate", true);
-        } catch (Exception e) {
-            e.printStackTrace();
-
-            // Creates Connection between "addCredentials()" Method & code that Displays Status of Duplicate Credentials inside "home.html" file
-            redirectAttributes.addFlashAttribute("add_credentials_not_duplicate", false);
         }
+        credentialsService.createCredentials(addCredentialsUrl, addCredentialsUsername, encodedKey, encryptedCredentialsPassword, users.getUserId());
+        allCredentials.add(newCredentials);
+
+        // Creates Connection between "addCredentials()" Method & code that Displays Status of Adding Credentials Successfully inside "home.html" file
+        redirectAttributes.addFlashAttribute("add_credentials_not_duplicate", true);
         
         model.addAttribute("files", this.fileService.getAllFilesByUserId(users.getUserId()));
         model.addAttribute("notes", this.noteService.getAllNotesByUserId(users.getUserId()));
