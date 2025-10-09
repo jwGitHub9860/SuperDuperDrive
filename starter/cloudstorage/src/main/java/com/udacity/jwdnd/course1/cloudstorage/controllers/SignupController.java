24896d7e3f7dec0286cd4f0e548fcf4f24144e0d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-08 22:34:01 -0400

    fix: Remove "@PostMapping" path because other method only displays sign up page
    Remove path for "@PostMapping" annotation inside "SignupController.java" file because other method only displays sign up page with no other functions

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/SignupController.java
@@ -29,7 +29,8 @@ public class SignupController {
         return "signup";
     }
 
-    @PostMapping("/credentials/{username}")
+    // Do NOT Need to Specify "@PostMapping" Path because Other Method ONLY DISPLAYS Sign Up Page, NO OTHER FUNCTIONS & there's Only 2 Methods
+    @PostMapping
     public String signUpForNewAccount(String username, RedirectAttributes redirectAttributes) {
         if (userService.isUsernameAvailable(username)) {
             redirectAttributes.addFlashAttribute("signup_error", false);
