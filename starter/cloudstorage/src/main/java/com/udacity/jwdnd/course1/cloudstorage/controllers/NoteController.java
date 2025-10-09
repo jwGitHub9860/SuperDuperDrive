16cecd3ebbb3d67034ae677aa2a984aaf0213791
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-10-09 18:53:01 -0400

    fix: Change "@RequestParam" paths to connect to "input" in "home.html"
    Change paths of "@RequestParam" for method input inside "NoteController.java" file to connect "@RequestParam" annotations to "input" inside "home.html" & new documentation inside "home.html" file to explain "name"

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/NoteController.java
@@ -32,7 +32,7 @@ public class NoteController {
     }
 
     @PostMapping("/addNewNote")
-    public String addNote(@RequestParam("addNote") String createNoteTitle, @RequestParam("addNote") Integer createNoteId, @RequestParam("addNote") String createNoteDescription, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+    public String addNote(@RequestParam("noteTitle") String createNoteTitle, @RequestParam("addNote") Integer createNoteId, @RequestParam("noteDescription") String createNoteDescription, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         Users users = userService.getUser(authentication.getName());
         Notes newNote = new Notes(createNoteTitle, createNoteId, createNoteDescription, users.getUserId());
 
@@ -59,7 +59,7 @@ public class NoteController {
     }
 
     @PostMapping("/notes/edit/{noteId}")
-    public String editNote(@RequestParam("chosenNote") String chosenNoteTitle, @RequestParam("chosenNote") Integer chosenNoteId, @RequestParam("chosenNote") String chosenNoteDescription, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+    public String editNote(@RequestParam("noteTitle") String chosenNoteTitle, @RequestParam("noteId") Integer chosenNoteId, @RequestParam("noteDescription") String chosenNoteDescription, Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
         Users users = userService.getUser(authentication.getName());
         noteService.editNote(chosenNoteTitle, chosenNoteDescription, chosenNoteId);
         
